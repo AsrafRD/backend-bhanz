08f145f1dd3cdd8e8946fc3ae2a3f24f8a06f435
AsrafRD <146421331+AsrafRD@users.noreply.github.com>
2024-06-02 19:24:56 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -30,11 +30,11 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "bambu-runcing-petshop",
+    password: "15jKFRQm3J2MMgtMR_Wyzw",
+    database: "bambu-runcing-petshop",
+    host: "dune-ox-6972.8nk.gcp-asia-southeast1.cockroachlabs.cloud",
+    port: "26257",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
