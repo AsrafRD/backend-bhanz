f669ab290fb449ea599d0f81e18ea6331440e9e7
AsrafRD <asrafraffi2019@gmail.com>
2024-06-02 19:15:05 +0700

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,18 @@
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgresql",
+    username: "bambu-runcing-petshop",
+    password: "15jKFRQm3J2MMgtMR_Wyzw",
+    database: "bambu-runcing-petshop",
+    host: "dune-ox-6972.8nk.gcp-asia-southeast1.cockroachlabs.cloud",
+    port: "26257",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: process.env.DB_USER,
@@ -15,7 +21,13 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "postgresql",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
     username: process.env.DB_USER,
@@ -23,6 +35,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "postgresql",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
